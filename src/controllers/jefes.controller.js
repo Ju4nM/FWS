28a67647f2e9de093a3ce519387c094002ab233a
Ju4nM <jenriquemflores@gmail.com>
2022-06-28 16:10:19 -0700

    Avance de login

--- src/controllers/jefes.controller.js
+++ src/controllers/jefes.controller.js
@@ -1,30 +1,36 @@
-import sql from 'mssql';
+// import sql from 'mssql';
 import getConnection from './../database/conection.js';
+import Cipher from '../utils/Cipher.js';
 
 // let result = await pool.request().query('SELECT 1');
 // console.log(result);
 
-/* Opciones para SP_Jefe
+/* 
+ * Opciones para SP_Jefe
  * 1 = Create (C)
  * 2 = Read (R)
  * 3 = Update (U)
  * 4 = Delete (D)
  * 5 = Login
+ * 
  */ 
 
-class Jefe {
+class Boss {
     constructor (getConnection) {
         this.pool = getConnection;
     }
     
-    async login (userName, password) {
-        return await this.pool.request()
-            .input('nombreUsuario', userName)
-            .input('contrasena', password)
-            .input('opcion', 5)
-            .execute("SP_Jefe");
+    async login (userName, plainPassword) {
+        const { recordset } = await this.pool.request()
+            .input('userName', userName)
+            .input('op', 5)
+            .execute("sp_boss");
+        
+        const { password } = recordset[0];
+
+        return await Cipher.compareHashes(plainPassword, password);
     }
 
 }
 
-export default new Jefe(await getConnection());
+export default new Boss(await getConnection());
