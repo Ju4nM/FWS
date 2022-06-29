1748c48b584065d339b053440ac3b80c9fa2d7da
Ju4nM <jenriquemflores@gmail.com>
2022-06-28 18:12:34 -0700

    Nuevos cambios

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import app from "../app.js";
 import login from '../controllers/login.js';
 
 const router = express.Router();
 
 router.post('/login', login);
+
+// router.get("/", (req, res) => {
+//     res.sendFile(path.resolve() + "\\public\\controlPanel.html");
+// })
+
+router.get("/dashboard", (req, res) => {
+    // res.sendFile(controlPanel.html");
+    // res.send(path.resolve + "\\public\\controlPanel.html");
+});
 
 export default router;
