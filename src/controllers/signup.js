7ac5bfde05257b9edbaae01238cb8bcc1a3c15f2
MisaSM <misaelsarabiamolina@gmail.com>
2022-07-05 20:20:54 -0700

    Funciones de validacion iniciadas

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,9 +1,11 @@
 import Boss from './jefes.controller.js'
-
+import Validation from '../utils/fieldValidation.js'
 async function signup (req,res) {
     console.log(req.body);
     let {signupName, signupLastName, signupSecondLastN, signupUserName, signupEmail, signupPasswd} = req.body
-    
+    for (let field in req.body) {
+        valNames()
+    }
     // let result = await Boss.signup(signupName, signupLastName, signupSecondLastN, signupUserName, signupEmail, signupPasswd)
 }
 export default signup;
--- src/utils/fieldValidation.js
+++ src/utils/fieldValidation.js
@@ -1,1 +1,15 @@
-class Validation
+class Validation {
+constructor() {
+    this.emptyField = [];
+}
+
+
+valNames(string) {
+    if (string == "") 
+    {
+        this.emptyField.push("Este campo esta vacio");
+    }
+}
+}
+
+export default new Validation();
